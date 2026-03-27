d74aed2823fe4bd984224e2f342081e1b8a52ab5
bhavyadivya1972-blip <bhavyadivya1972@gmail.com>
2026-03-27 17:24:37 +0530

    Building a report card printer
    Updated input prompts to include colons for clarity.

--- 1_code.py
+++ 1_code.py
@@ -1,12 +1,12 @@
-name = input("Enter your name")
+name = input("Enter your name: ")
 print(name, type(name))
 
-is_student = bool(input("Enter True or False"))
+is_student = bool(input("Enter True or False: "))
 print(is_student, type(is_student))
 
-age = int(input("Enter your age"))
+age = int(input("Enter your age: "))
 print(age, type(age))
 
-score = float(input("Enter your score"))
+score = float(input("Enter your score: "))
 print(isinstance(score, float))
 print(score, type(score))
