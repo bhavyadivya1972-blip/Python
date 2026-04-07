13b52fe5449a8223d1e8f830bb46299665d3b189
bhavyadivya1972-blip <bhavyadivya1972@gmail.com>
2026-04-07 19:36:55 +0530

    Lists in Python
    This file contains "Lists in Python"

---
+++ 14_code.py
@@ -0,0 +1,7 @@
+"""  Lists in Python """
+
+l1 = [1, 2, 3, 4, 5]
+print(l1)  # Output: [1, 2, 3, 4, 5]
+
+l2 = ["apple", "banana", "cherry" , True, False, None, 3.14, [1, 2, 3]]
+print(l2)  # Output: ['apple', 'banana', 'cherry', True, False, None, 3.14, [1, 2, 3]]
