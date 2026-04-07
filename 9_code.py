baf912513170d17ab0382a0d3cc3c27fcf41d5a4
bhavyadivya1972-blip <bhavyadivya1972@gmail.com>
2026-04-07 19:27:45 +0530

    Strings in Python
    This file contains "Strings in Python"

---
+++ 9_code.py
@@ -0,0 +1,14 @@
+''' Strings in Python '''
+
+str1 = "Hello, World!" # This is a single line string
+str2 = ''' Python       # This is a multi-line string
+is
+great! '''
+
+print(str1) # Output: Hello, World!
+print(str2) # Output: Python
+              #         is
+              #         great!
+              
+
+
