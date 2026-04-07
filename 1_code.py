3ab70e65c3277174a31efe08d188abfe0d832a67
bhavyadivya1972-blip <bhavyadivya1972@gmail.com>
2026-04-07 19:20:12 +0530

    Comments in Python
    This file contains "Comments in Python"

---
+++ 1_code.py
@@ -0,0 +1,20 @@
+''' Python Comments '''
+''' There are two types of comments
+
+1. Single-line comment
+2. Multi-line comment '''
+
+#Single-line comment(#)
+
+# This is a single-line comment
+
+# Multi-line comment(""""""/'''''')
+
+''' This is 
+a 
+multi-line 
+comment '''
+
+
+
+
