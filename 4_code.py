74b7b299862d76d72c2987b74f93a4824cc2aa28
bhavyadivya1972-blip <bhavyadivya1972@gmail.com>
2026-04-07 19:23:07 +0530

    Numbers in Python
    This file contains "Numbers in Python"

---
+++ 4_code.py
@@ -0,0 +1,25 @@
+''' Python Numbers '''
+
+""" There are three types of numbers in Python:
+1. int --> 10, -5, 0
+2. float --> 10.5, -5.2, 0.0
+3. complex --> 1+2j, 3-4j """
+
+""" Integer Data Type """
+
+print(type(10)) # Output: <class 'int'>
+print(type(-5)) # Output: <class 'int'>
+print(type(0)) # Output: <class 'int'>
+
+""" Float Data Type """
+
+print(type(10.5)) # Output: <class 'float'>
+print(type(-5.2)) # Output: <class 'float'>
+print(type(0.0)) # Output: <class 'float'>
+print(type(1e-3)) # Exponential notation # Output: <class 'float'>
+print(type(1e+3)) # Exponential notation # Output: <class 'float'>
+
+""" Complex Data Type """
+
+print(type(1+2j)) # Output: <class 'complex'>
+print(type(3-4j)) # Output: <class 'complex'>
