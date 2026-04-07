b6b44d216daddee4cbf234d9dc8f2f70c08b7806
bhavyadivya1972-blip <bhavyadivya1972@gmail.com>
2026-04-07 19:26:59 +0530

    Booleans in Python
    This file contains "Booleans in Python"

---
+++ 8_code.py
@@ -0,0 +1,16 @@
+''' Booleans in Python '''
+
+a = True
+b = False
+
+# print(a)
+# print(b)
+
+print(5>4) # Output: True
+print(5<4) # Output: False
+print(bool(0)) # Output: False
+print(bool(198053)) # Output: True
+print(bool([])) # Output: False
+print(bool([1, 2, 3])) # Output: True
+print(bool(" ")) # Output: True
+print(bool("Hello")) # Output: True
