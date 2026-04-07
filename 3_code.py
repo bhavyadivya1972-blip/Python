e1fb83b13339d6caa9637e7f5493448a14de39d6
bhavyadivya1972-blip <bhavyadivya1972@gmail.com>
2026-04-07 19:22:13 +0530

    Data Types in Python
    This file contains "Data Types in Python"

---
+++ 3_code.py
@@ -0,0 +1,36 @@
+''' Python Data Types '''
+''' There are various data types in Python, such as:
+1. int --> 10,-5,0
+2. float --> 10.5,-5.2,0.0
+3. complex --> 1+2j,3-4j
+4. str --> "Hello, World!"
+5. bool --> True, False
+6. list --> [1, 2, 3]
+7. tuple --> (1, 2, 3)
+8. set --> {1, 2, 3}
+9. dict --> {"key": "value"},{"name": "Abhay", "age": 20}
+10. NoneType --> None '''
+
+print(type(10)) # Output: <class 'int'>
+print(type(10.5)) # Output: <class 'float'>
+print(type(1+2j)) # Output: <class 'complex'>
+print(type("Hello, World!")) # Output: <class 'str'>
+print(type(True)) # Output: <class 'bool'>
+print(type([1, 2, 3])) # Output: <class 'list'>
+print(type((1, 2, 3))) # Output: <class 'tuple'>
+print(type({1, 2, 3})) # Output: <class 'set'>
+print(type({"key": "value"})) # Output: <class 'dict'>
+print(type(None)) # Output: <class 'NoneType'>
+
+""" String Data Type """
+
+print(type('10')) # Output: <class 'str'>
+print(type('10.5')) # Output: <class 'str'>
+print(type('1+2j')) # Output: <class 'str'>
+print(type("Hello, World!")) # Output: <class 'str'>
+print(type('True')) # Output: <class 'str'>
+print(type("1, 2, 3")) # Output: <class 'str'>
+print(type("key: value")) # Output: <class 'str'>
+print(type("None")) # Output: <class 'str'>
+print(type("*&^%$#@!|*-+/`~(){}[]\2")) # Output: <class 'str'>
+
