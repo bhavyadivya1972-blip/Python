2a582df84e17ed0fcf94f49fbb61f074b22ae24e
bhavyadivya1972-blip <bhavyadivya1972@gmail.com>
2026-04-07 19:28:37 +0530

    Operation with strings
    This file contains "Operation with strings"

---
+++ 10_code.py
@@ -0,0 +1,22 @@
+''' Operation with Strings '''
+
+""" 1. Length of a string """
+
+str1 = "Hello"
+print(len(str1)) # Output: 5
+
+""" 2. String as an array"""
+
+str2 = "Hello"
+print(str2[1:5]) # Output: ello
+
+""" 3. Loop through a string"""
+
+for i in str1:
+    print(i)  # Output:
+              # H
+              # e
+              # l
+              # l
+              # o
+
