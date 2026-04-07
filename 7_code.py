3f3e9ae0a553056974efcb434482e7cc50833301
bhavyadivya1972-blip <bhavyadivya1972@gmail.com>
2026-04-07 19:26:05 +0530

    Operators in Python
    This file contains "Operators in Python"

---
+++ 7_code.py
@@ -0,0 +1,11 @@
+''' Opeartors in Python '''
+
+""" There are several types of operators in Python, including:
+1. Arithmetic Operators: +, -, *, /, %, **, //
+2. Comparison Operators: ==, !=, >, <, >=, <=
+3. Assignment Operators: =, +=, -=, *=, /=, %=, **=, //=
+4. Logical Operators: and, or, not
+5. Bitwise Operators: &, |, ^, ~, <<, >>
+6. Membership Operators: in, not in
+7. Identity Operators: is, is not
+8. Ternary Operator: if-else """
