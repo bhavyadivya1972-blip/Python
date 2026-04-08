320dce5a9488ade51bea36bf489b357fc17f4041
bhavyadivya1972-blip <bhavyadivya1972@gmail.com>
2026-04-08 18:28:13 +0530

    Nested Loops
    This file contains "Nested Loops"

---
+++ 39_code.py
@@ -0,0 +1,20 @@
+''' Nested Loops in Python '''
+
+for i in range(2,3):
+    for j in range(1,11):
+        print(f"{i} x {j} = {i*j}")
+        
+# Output:
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# 2 x 4 = 8
+# 2 x 5 = 10
+# 2 x 6 = 12
+# 2 x 7 = 14
+# 2 x 8 = 16
+# 2 x 9 = 18
+# 2 x 10 = 20
+     
+    
+    
