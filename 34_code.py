3ef75eda4085c3285527dbb179ec4a28b76ae4fd
bhavyadivya1972-blip <bhavyadivya1972@gmail.com>
2026-04-08 18:06:53 +0530

    If-else Statement
    This file contains "If-else Statement"

---
+++ 34_code.py
@@ -0,0 +1,11 @@
+''' If-else statements '''
+
+x = 10
+
+if x>10:
+    print("x is greater than 10")
+    
+else:
+    print("x is less than or equal to 10")
+    
+# Output: x is less than or equal to 10
