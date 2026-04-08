8297a56f451cab1ff2f394bbc45c993a9f804a6e
bhavyadivya1972-blip <bhavyadivya1972@gmail.com>
2026-04-08 18:26:33 +0530

    For Loop in Python
    This file contains "For Loop in Python"

---
+++ 37_code.py
@@ -0,0 +1,19 @@
+''' For loop in Python '''
+
+for i in range(5):
+    print(i)
+# Output: 0
+#         1
+#         2
+#         3
+#         4
+
+name = "Alice"
+for char in name:
+    print(char)
+# Output: A
+#         l
+#         i
+#         c
+#         e
+
