9f8bf5bc3af244ddabe06c642b4dc9bf8c00d460
bhavyadivya1972-blip <bhavyadivya1972@gmail.com>
2026-04-08 18:07:39 +0530

    elif Statement
    This file contains "elif Statement"

---
+++ 35_code.py
@@ -0,0 +1,14 @@
+''' elif Statements '''
+
+x = 100
+
+if x>100:
+    print("x is greater than 100")
+    
+elif x == 100:
+    print("x is equal to 100")
+    
+else:
+    print("x is less than 100")
+    
+# Output: x is equal to 100
